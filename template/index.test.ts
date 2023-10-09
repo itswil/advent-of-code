e79ef125a4b654e7f8c3c4044c80e2a2283dc077
itswil <ww8668@gmail.com>
2023-10-09 17:29:35 +0800

    Comment out test in template

--- template/index.test.ts
+++ template/index.test.ts
@@ -5,8 +5,8 @@ const PATH = import.meta.dir;
 const testInput = Bun.file(`${PATH}/input-test.txt`);
 const input = Bun.file(`${PATH}/input.txt`);
 
-test("should return ...", async () => {
-  expect(getValue(await testInput.text())).toBe(88888888);
+// test("should return ...", async () => {
+//   expect(getValue(await testInput.text())).toBe(88888888);
 
-  console.log("🌟 Answer:", getValue(await input.text()));
-});
+//   console.log("🌟 Answer:", getValue(await input.text()));
+// });
