40ab0263b3e24bc079d5ce646ef6f2a0bcdf8b71
itswil <ww8668@gmail.com>
2023-11-30 12:54:49 +0800

    Add 2016/07 part B

--- 2016/07/index.test.ts
+++ 2016/07/index.test.ts
@@ -1,14 +1,22 @@
 import { expect, test } from "bun:test";
-import { getNumberOfIps } from ".";
+import { getNumberOfSslIps, getNumberOfTlsIps } from ".";
 
 const PATH = import.meta.dir;
 const testInputFile = Bun.file(`${PATH}/input-test.txt`);
+const testInputFile2 = Bun.file(`${PATH}/input-test2.txt`);
 const inputFile = Bun.file(`${PATH}/input.txt`);
 const testInput = await testInputFile.text();
+const testInput2 = await testInputFile2.text();
 const input = await inputFile.text();
 
 test("should return number of TLS-supporting IPs", () => {
-  expect(getNumberOfIps(testInput)).toBe(2);
+  expect(getNumberOfTlsIps(testInput)).toBe(2);
 
-  console.log("🌟 Answer:", getNumberOfIps(input));
+  console.log("🌟 Answer:", getNumberOfTlsIps(input));
+});
+
+test("should return number of SSL-supporting IPs", () => {
+  expect(getNumberOfSslIps(testInput2)).toBe(3);
+
+  console.log("🌟 Answer:", getNumberOfSslIps(input));
 });
--- 2016/07/index.ts
+++ 2016/07/index.ts
@@ -1,10 +1,11 @@
-type IpSegment = Array<string>;
-type SegmentedIp = [IpSegment, IpSegment];
+type IpSegment = string;
+type IpSegments = Array<IpSegment>;
+type SegmentedIp = [IpSegments, IpSegments];
 
 const splitIp = (ip: string): SegmentedIp => {
   const segments = ip.replaceAll("[", "-").replaceAll("]", "-").split("-");
-  const standardSegments: IpSegment = [];
-  const hypernetSegments: IpSegment = [];
+  const standardSegments: IpSegments = [];
+  const hypernetSegments: IpSegments = [];
 
   for (let i = 0; i < segments.length; i++) {
     if (i % 2 === 0) {
@@ -17,7 +18,7 @@ const splitIp = (ip: string): SegmentedIp => {
   return [standardSegments, hypernetSegments];
 };
 
-export const containsAbbaSequence = (segment: string): boolean => {
+export const containsABBASequence = (segment: string): boolean => {
   for (let i = 0; i < segment.length - 3; i++) {
     if (
       segment[i] === segment[i + 3] &&
@@ -31,7 +32,7 @@ export const containsAbbaSequence = (segment: string): boolean => {
   return false;
 };
 
-export const getNumberOfIps = (input: string): number => {
+export const getNumberOfTlsIps = (input: string): number => {
   const ips = input.split("\n");
   let numberOfValidIps = 0;
 
@@ -39,8 +40,56 @@ export const getNumberOfIps = (input: string): number => {
     const [standardSegments, hypernetSegments] = splitIp(ip);
 
     if (
-      !hypernetSegments.some(containsAbbaSequence) &&
-      standardSegments.some(containsAbbaSequence)
+      !hypernetSegments.some(containsABBASequence) &&
+      standardSegments.some(containsABBASequence)
+    ) {
+      numberOfValidIps++;
+    }
+  }
+
+  return numberOfValidIps;
+};
+
+type BAB = string;
+type BABs = Array<BAB>;
+const getAllBABs = (segments: IpSegments): BABs => {
+  const allBABs: BABs = [];
+
+  for (const segment of segments) {
+    for (let i = 0; i < segment.length - 2; i++) {
+      if (segment[i] === segment[i + 2] && segment[i] !== segment[i + 1]) {
+        allBABs.push(segment.slice(i, i + 3));
+      }
+    }
+  }
+
+  return allBABs;
+};
+
+const containsAnyABASequence = (segment: IpSegment, allBABs: BABs): boolean => {
+  for (const bab of allBABs) {
+    const ABA = bab[1] + bab[0] + bab[1];
+    if (segment.includes(ABA)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+export const getNumberOfSslIps = (input: string): number => {
+  const ips = input.split("\n");
+  let numberOfValidIps = 0;
+
+  for (const ip of ips) {
+    const [standardSegments, hypernetSegments] = splitIp(ip);
+
+    const allBABs = getAllBABs(hypernetSegments);
+
+    if (
+      standardSegments.some((segment) =>
+        containsAnyABASequence(segment, allBABs)
+      )
     ) {
       numberOfValidIps++;
     }
