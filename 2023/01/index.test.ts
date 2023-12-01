8a9e6f3a576e5b5414a356d3bd9a265a0894692e
itswil <ww8668@gmail.com>
2023-12-01 18:30:28 +0800

    Add 2023/01 Part B

--- 2023/01/index.test.ts
+++ 2023/01/index.test.ts
@@ -1,14 +1,22 @@
 import { expect, test } from "bun:test";
-import { getCalibrationValueSum } from ".";
+import { getCalibrationValueSum, getCalibrationValueSumV2 } from ".";
 
 const PATH = import.meta.dir;
 const testInputFile = Bun.file(`${PATH}/input-test.txt`);
+const testInputFile2 = Bun.file(`${PATH}/input-test2.txt`);
 const inputFile = Bun.file(`${PATH}/input.txt`);
 const testInput = await testInputFile.text();
+const testInput2 = await testInputFile2.text();
 const input = await inputFile.text();
 
-test("should return ...", () => {
+test("should return the sum of first and last digits", () => {
   expect(getCalibrationValueSum(testInput)).toBe(142);
 
   console.log("🌟 Answer:", getCalibrationValueSum(input));
+});
+
+test("should return the sum of first and last numbers (digits or words)", () => {
+  expect(getCalibrationValueSumV2(testInput2)).toBe(281);
+
+  console.log("🌟 Answer:", getCalibrationValueSumV2(input));
 });
--- 2023/01/index.ts
+++ 2023/01/index.ts
@@ -26,11 +26,99 @@ export const getCalibrationValueSum = (input: string): number => {
 
   for (const line of lines) {
     const firstDigit = getFirstDigit(line);
-    console.log("🚀 ~ getCalibrationValueSum ~ firstDigit:", firstDigit);
     const lastDigit = getLastDigit(line);
-    console.log("🚀 ~ getCalibrationValueSum ~ lastDigit:", lastDigit);
 
     values.push(parseInt(firstDigit + lastDigit, 10));
+  }
+
+  return values.reduce((a, c) => a + c, 0);
+};
+
+const NUMBERS = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+} as const;
+
+const getFirstNumber = (code: string): keyof typeof NUMBERS => {
+  let savedIndex;
+  let numberFound: keyof typeof NUMBERS;
+  for (const number of Object.keys(NUMBERS)) {
+    const indexOfNumber = code.indexOf(number);
+    if (indexOfNumber >= 0) {
+      if (savedIndex === undefined) {
+        numberFound = number as keyof typeof NUMBERS;
+        savedIndex = indexOfNumber;
+      } else if (indexOfNumber < savedIndex) {
+        numberFound = number as keyof typeof NUMBERS;
+        savedIndex = indexOfNumber;
+      }
+    }
+  }
+
+  return numberFound!;
+};
+
+const getLastNumber = (code: string): keyof typeof NUMBERS => {
+  let savedIndex;
+  let numberFound: keyof typeof NUMBERS;
+  for (const number of Object.keys(NUMBERS)) {
+    const lastIndexOfNumber = code.lastIndexOf(number);
+    if (lastIndexOfNumber >= 0) {
+      if (savedIndex === undefined) {
+        numberFound = number as keyof typeof NUMBERS;
+        savedIndex = lastIndexOfNumber;
+      } else if (lastIndexOfNumber > savedIndex) {
+        numberFound = number as keyof typeof NUMBERS;
+        savedIndex = lastIndexOfNumber;
+      }
+    }
+  }
+
+  return numberFound!;
+};
+
+export const getCalibrationValueSumV2 = (input: string): number => {
+  const values = [];
+  const lines = input.split("\n");
+
+  for (const line of lines) {
+    const firstDigit = getFirstDigit(line);
+    const lastDigit = getLastDigit(line);
+    const firstNumber = getFirstNumber(line);
+    const lastNumber = getLastNumber(line);
+    console.log("111", firstDigit, lastDigit, firstNumber, lastNumber);
+
+    let first;
+    let last;
+
+    if (
+      (firstDigit && line.indexOf(firstDigit) < line.indexOf(firstNumber)) ||
+      !firstNumber
+    ) {
+      first = firstDigit;
+    } else {
+      first = NUMBERS[firstNumber];
+    }
+
+    if (
+      (lastDigit &&
+        line.lastIndexOf(lastDigit) > line.lastIndexOf(lastNumber)) ||
+      !lastNumber
+    ) {
+      last = lastDigit;
+    } else {
+      last = NUMBERS[lastNumber];
+    }
+
+    console.log("🚀", first, last);
+    values.push(parseInt(first + last, 10));
   }
 
   return values.reduce((a, c) => a + c, 0);
