177a363ae1a36908fcdace8aacc3aa3288d9b431
itswil <ww8668@gmail.com>
2023-12-13 19:55:49 +0800

    Add 2023/08 part B

--- 2023/08/index.test.ts
+++ 2023/08/index.test.ts
@@ -1,12 +1,14 @@
 import { expect, test } from "bun:test";
-import { getNumberOfSteps } from ".";
+import { getNumberOfSteps, getNumberOfStepsV2, isAllEndInZ } from ".";
 
 const PATH = import.meta.dir;
 const testInputFile = Bun.file(`${PATH}/input-test.txt`);
 const testInputFile2 = Bun.file(`${PATH}/input-test2.txt`);
+const testInputFile3 = Bun.file(`${PATH}/input-test3.txt`);
 const inputFile = Bun.file(`${PATH}/input.txt`);
 const testInput = await testInputFile.text();
 const testInput2 = await testInputFile2.text();
+const testInput3 = await testInputFile3.text();
 const input = await inputFile.text();
 
 test("should return the number of steps needed to reach ZZZ", () => {
@@ -14,4 +16,16 @@ test("should return the number of steps needed to reach ZZZ", () => {
   expect(getNumberOfSteps(testInput2)).toBe(6);
 
   console.log("🌟 Answer:", getNumberOfSteps(input));
+});
+
+test.skip("should return true if all positions end in Z", () => {
+  expect(isAllEndInZ(["AAZ", "BBZ", "ZZZ"])).toBe(true);
+  expect(isAllEndInZ(["AAA", "BBZ", "ZZZ"])).toBe(false);
+});
+
+// Skipped: runs in 51s
+test.skip("should return the number of steps needed to reach **Z", () => {
+  expect(getNumberOfStepsV2(testInput3)).toBe(6);
+
+  console.log("🌟 Answer:", getNumberOfStepsV2(input));
 });
--- 2023/08/index.ts
+++ 2023/08/index.ts
@@ -29,3 +29,101 @@ export const getNumberOfSteps = (input: string): number => {
 
   return steps;
 };
+
+const getStartPositions = (nodes: Record<string, string>): Array<string> =>
+  Object.keys(nodes).filter((position) => position.at(-1) === "A");
+
+export const isAllEndInZ = (currentPositions: Array<string>): boolean =>
+  currentPositions.filter((pos) => pos.at(-1) === "Z").length ===
+  currentPositions.length;
+
+// This does not complete in any reasonable time
+const getNumberOfStepsV2_BruteForce = (input: string): number => {
+  let steps = 0;
+  let pointer = 0;
+  const [instructions, network] = input.split("\n\n");
+  const nodes: Record<string, string> = Object.fromEntries(
+    network
+      .split("\n")
+      .map((node) => node.replace("(", "").replace(")", "").split(" = "))
+  );
+  let currentPositions = getStartPositions(nodes);
+
+  while (!isAllEndInZ(currentPositions)) {
+    if (instructions[pointer] === "L") {
+      currentPositions.forEach(
+        (pos, index, array) => (array[index] = nodes[pos].slice(0, 3))
+      );
+    } else {
+      currentPositions.forEach(
+        (pos, index, array) => (array[index] = nodes[pos].slice(-3))
+      );
+    }
+
+    steps++;
+    if (pointer === instructions.length - 1) {
+      pointer = 0;
+    } else {
+      pointer++;
+    }
+  }
+
+  return steps;
+};
+
+export const getLowestCommonMultiple = (numbers: Array<number>): number => {
+  const sortedNumbers = numbers.toSorted();
+  const biggestNumber = sortedNumbers.pop()!;
+  let multiple = 1;
+  let isCommonMultiple = true;
+
+  while (true) {
+    const target = biggestNumber * multiple;
+
+    for (const num of sortedNumbers) {
+      if (target % num !== 0) {
+        isCommonMultiple = false;
+        break;
+      }
+    }
+
+    if (isCommonMultiple) {
+      return target;
+    }
+
+    multiple++;
+    isCommonMultiple = true;
+  }
+};
+
+export const getNumberOfStepsV2 = (input: string): number => {
+  const [instructions, network] = input.split("\n\n");
+  const nodes: Record<string, string> = Object.fromEntries(
+    network
+      .split("\n")
+      .map((node) => node.replace("(", "").replace(")", "").split(" = "))
+  );
+  let startPositions = getStartPositions(nodes);
+  let steps = Array(startPositions.length).fill(0);
+
+  startPositions.forEach((position, index) => {
+    let currentPosition = position;
+    let pointer = 0;
+    while (currentPosition.at(-1) !== "Z") {
+      if (instructions[pointer] === "L") {
+        currentPosition = nodes[currentPosition].slice(0, 3);
+      } else {
+        currentPosition = nodes[currentPosition].slice(-3);
+      }
+
+      steps[index]++;
+      if (pointer === instructions.length - 1) {
+        pointer = 0;
+      } else {
+        pointer++;
+      }
+    }
+  });
+
+  return getLowestCommonMultiple(steps);
+};
