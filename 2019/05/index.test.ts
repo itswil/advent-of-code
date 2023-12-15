4db3517c4e3a5d745429745c8a31e40562acc5a7
itswil <ww8668@gmail.com>
2023-12-15 14:05:12 +0800

    Add 2019/05 part B

--- 2019/05/index.test.ts
+++ 2019/05/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test";
-import { getDiagnosticCode } from ".";
+import { getDiagnosticCode, getDiagnosticCodeV2 } from ".";
 
 const PATH = import.meta.dir;
 const testInputFile = Bun.file(`${PATH}/input-test.txt`);
@@ -7,8 +7,25 @@ const inputFile = Bun.file(`${PATH}/input.txt`);
 const testInput = await testInputFile.text();
 const input = await inputFile.text();
 
-test("should return the diagnostic code from the program", () => {
+test("should return the diagnostic code from the program using value `1`", () => {
   // No test cases available
 
   console.log("🌟 Answer:", getDiagnosticCode(input, 1));
+});
+
+test("should return the diagnostic code from the program using value `5`", () => {
+  const positionModeTestInput = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9";
+  const immediateModeTestInput = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1";
+
+  expect(getDiagnosticCodeV2(positionModeTestInput, 0)).toBe(0);
+  expect(getDiagnosticCodeV2(positionModeTestInput, 2)).toBe(1);
+
+  expect(getDiagnosticCodeV2(immediateModeTestInput, 0)).toBe(0);
+  expect(getDiagnosticCodeV2(immediateModeTestInput, 2)).toBe(1);
+
+  expect(getDiagnosticCodeV2(testInput, 7)).toBe(999);
+  expect(getDiagnosticCodeV2(testInput, 8)).toBe(1000);
+  expect(getDiagnosticCodeV2(testInput, 9)).toBe(1001);
+
+  console.log("🌟 Answer:", getDiagnosticCodeV2(input, 5));
 });
--- 2019/05/index.ts
+++ 2019/05/index.ts
@@ -59,3 +59,93 @@ export const getDiagnosticCode = (input: string, value: number): number => {
     }
   }
 };
+
+const isJumpIfTrueOp = (code: number) => code === 5;
+const isJumpIfFalseOp = (code: number) => code === 6;
+const isLessThanOp = (code: number) => code === 7;
+const isEqualsOp = (code: number) => code === 8;
+
+export const getDiagnosticCodeV2 = (input: string, value: number): number => {
+  const ops = input.split(",").map(Number); // list of operations
+
+  let i = 0; // pointer
+
+  while (true) {
+    let op = ops[i];
+    let a = ops[i + 1];
+    let b = ops[i + 2];
+    let c = ops[i + 3];
+
+    if (isImmediateMode(op)) {
+      const opStr = `${op}`.padStart(5, "0");
+      const [aMode, bMode, cMode, ...opCode] = opStr.split("");
+
+      op = parseInt(opCode.join(""));
+
+      if (parseInt(aMode)) {
+        c = i + 3;
+      }
+      if (parseInt(bMode)) {
+        b = i + 2;
+      }
+      if (parseInt(cMode)) {
+        a = i + 1;
+      }
+    }
+
+    if (isHaltOp(op)) {
+      // console.log("HALT");
+      return value;
+    } else if (isAddOp(op)) {
+      // console.log(`ADD: ${ops[c]}`);
+      ops[c] = ops[a] + ops[b];
+      // console.log(`-> ${ops[c]}`);
+      i = i + 4;
+    } else if (isMultiplyOp(op)) {
+      // console.log(`MULTIPLY: ${ops[c]}`);
+      ops[c] = ops[a] * ops[b];
+      // console.log(`-> ${ops[c]}`);
+      i = i + 4;
+    } else if (isStoreOp(op)) {
+      // console.log(`STORE: ${ops[a]}`);
+      ops[a] = value;
+      // console.log(`-> ${ops[a]}`);
+      i = i + 2;
+    } else if (isOutputOp(op)) {
+      // console.log(`OUTPUT: ${ops[a]}`);
+      value = ops[a];
+      // console.log(`-> ${ops[a]}`);
+      i = i + 2;
+    } else if (isJumpIfTrueOp(op)) {
+      // console.log(`JUMP: ${ops[a]} not equals 0? -> ${ops[b]}`);
+      if (ops[a] !== 0) {
+        i = ops[b];
+      } else {
+        i = i + 3;
+      }
+    } else if (isJumpIfFalseOp(op)) {
+      // console.log(`JUMP: ${ops[a]} equals 0? -> ${ops[b]}`);
+      if (ops[a] === 0) {
+        i = ops[b];
+      } else {
+        i = i + 3;
+      }
+    } else if (isLessThanOp(op)) {
+      // console.log(`LESS THAN: ${ops[a]} ${ops[b]}`);
+      if (ops[a] < ops[b]) {
+        ops[c] = 1;
+      } else {
+        ops[c] = 0;
+      }
+      i = i + 4;
+    } else if (isEqualsOp(op)) {
+      // console.log(`EQUALS: ${ops[a]} ${ops[b]}`);
+      if (ops[a] === ops[b]) {
+        ops[c] = 1;
+      } else {
+        ops[c] = 0;
+      }
+      i = i + 4;
+    }
+  }
+};
